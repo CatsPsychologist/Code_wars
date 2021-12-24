f41cc409acc5ca8542eeedd23a4f0ef152bf9359
CatsPsychologist <mainofficee@gmail.com>
2021-12-24 15:10:11 +0200

    in

---
+++ FirstOne/js/script.js
@@ -0,0 +1,11 @@
+function shortName(str){
+
+    // return str.split(' ').splice(0,1).toUpperCase().join(' ');
+    return str.split(' ')
+        .map(function (el){
+        return el.slice(0,1).toUpperCase() + ('.');
+    }).join(' ')
+}
+
+let a = 'Golovach Lena';
+console.log(shortName(a));
