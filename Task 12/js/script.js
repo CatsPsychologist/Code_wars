3b2b76db03c6aa9033b23141fc1c98214caddd04
CatsPsychologist <mainofficee@gmail.com>
2022-01-11 11:45:04 +0200

    in

--- Task 12/js/script.js
+++ Task 12/js/script.js
@@ -1,6 +1,7 @@
 const unique = (array) => {
-    throw new Error('Напишите здесь свое решение');
+    let newArr = new Set(array);
+    return newArr;
 }
 
 const data = [1, 2, 1, 2, 3];
-console.log(unique(data)); // [1, 2, 3]
+console.log(unique(data));
