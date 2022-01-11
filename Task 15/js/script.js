7437f84620c32e835ae0f92ef5bbbc8dccf227a7
CatsPsychologist <mainofficee@gmail.com>
2022-01-11 18:40:49 +0200

    in

---
+++ Task 15/js/script.js
@@ -0,0 +1,7 @@
+const chunk = (array, size) => {
+    throw new Error('Напишите здесь свое решение');
+}
+
+const data = [1, 2, 3, 4, 5, 6, 7];
+console.log(chunk(data, 2)) // [[1, 2], [3, 4], [5, 6], [7]]
+console.log(chunk(data, 3))
