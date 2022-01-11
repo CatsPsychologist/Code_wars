62413277c740b08ce35bc83e10476fb047e7ca04
CatsPsychologist <mainofficee@gmail.com>
2022-01-11 18:38:02 +0200

    in

--- Task 14/js/script.js
+++ Task 14/js/script.js
@@ -1,7 +1,20 @@
-const flatten = (array) => {
-        let updArr = array.flat()
-    return updArr.flat()
+// const flatten = (array) => {
+//         let updArr = array.flat()
+//     return updArr.flat()
+//
+// }
+// const data = [1, 2, [3, 4, [5]]];
+// console.log(flatten(data));
 
-}
-const data = [1, 2, [3, 4, [5]]];
-console.log(flatten(data));
+const arr = [1, 2, 3, 4, 5];
+
+// function duplicate(arr){
+//     // let updArr = [];
+//     // return updArr = arr.concat(arr);
+//     return arr.concat(arr);
+//
+// }
+
+const duplicate2 = arr => arr.concat(arr)
+
+console.log(duplicate2(arr))
