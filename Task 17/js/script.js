e3de6985256f803c2a7e3321335acb1a0cbd0e5c
CatsPsychologist <mainofficee@gmail.com>
2022-01-13 14:12:54 +0200

    in

--- Task 17/js/script.js
+++ Task 17/js/script.js
@@ -2,8 +2,15 @@ const first = [1, 2, 3, 4];
 const second = [3, 4, 5 ,6];
 
 function intersection(a, b){
-    let c = a.concat(b);
+    let c = a.concat(b).sort();
     console.log(c)
+    c.reduce((prev, next) => {
+        let arr= []
+        if (prev === next){
+            return arr.push(prev);
+
+        }
+    })
 
 }
 
