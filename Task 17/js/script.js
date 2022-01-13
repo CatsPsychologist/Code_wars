ca6dfe7d00df5811e91148d6ea99b70a1fb70290
CatsPsychologist <mainofficee@gmail.com>
2022-01-13 14:50:16 +0200

    in

--- Task 17/js/script.js
+++ Task 17/js/script.js
@@ -1,17 +1,23 @@
-const first = [1, 2, 3, 4];
-const second = [3, 4, 5 ,6];
+// const first = [1, 2, 3, 4];
+// const second = [3, 4, 5 ,6];
+//
+// function intersection(a, b){
+//     let c = a.concat(b).sort();
+//     console.log(c)
+//     c.reduce((prev, next) => {
+//         let arr= []
+//         if (prev === next){
+//             return arr.push(prev);
+//
+//         }
+//     })
+//
+// }
 
-function intersection(a, b){
-    let c = a.concat(b).sort();
-    console.log(c)
-    c.reduce((prev, next) => {
-        let arr= []
-        if (prev === next){
-            return arr.push(prev);
+// intersection(first, second);
 
-        }
-    })
-
+for(var i = 0; i < 10; i ++){
+    setTimeout(function (){
+        console.log(i);
+    }, 0);
 }
-
-intersection(first, second)
