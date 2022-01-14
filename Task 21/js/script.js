0d0bccd154cd278020860868a658d43dd242a636
CatsPsychologist <mainofficee@gmail.com>
2022-01-14 20:48:18 +0200

    in

--- Task 21/js/script.js
+++ Task 21/js/script.js
@@ -2,10 +2,11 @@ const createPhoneNumber = numbers =>{
     if(numbers.length !== 10){
         return 'Please, make sure you put exactly 10 numbers'
     }
-    let updArr = numbers.toString().replace(',', ' ')
-
-    return numbers
+    let updArray = [numbers.join('')];
+    // updArray.splice(1, 0, '(')
+    return a
 }
 
 console.log(createPhoneNumber([1]));
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+console.log(2);
