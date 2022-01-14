35d901d13711083c290e1a6ad2f3fbb0605afba2
CatsPsychologist <mainofficee@gmail.com>
2022-01-14 18:40:44 +0200

    in

---
+++ Task 20/js/script.js
@@ -0,0 +1,11 @@
+// const oddOrEven = array => {
+//     if (JSON.stringify(array) === 0 )
+// }
+
+// console.log(oddOrEven([0]));
+// console.log(oddOrEven([1]));
+// console.log(oddOrEven([]));
+
+let arr = [0]
+
+console.log(JSON.stringify(arr) === 0)
