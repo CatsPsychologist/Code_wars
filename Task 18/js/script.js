090867ff3b4a53df93d9061c63c4b1edea787a2e
CatsPsychologist <mainofficee@gmail.com>
2022-01-14 11:19:32 +0200

    in

--- Task 18/js/script.js
+++ Task 18/js/script.js
@@ -1,15 +1,7 @@
 function disemvowel(str) {
 
-    // const vol = 'e, u, i, o, a ,';
-    // let arr = Array.from(str);
-    // console.log(arr)
-    // arr.filter(value => value === 't')
-
     let newStr = str.replace(/[aeiou]+/g, '')
-
    return newStr
-
-
 }
 
 console.log(disemvowel("This website is for losers LOL!"));
