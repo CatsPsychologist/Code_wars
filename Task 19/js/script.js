09718ded15c9d99fba212d9c30f21d71910097ea
CatsPsychologist <mainofficee@gmail.com>
2022-01-14 18:30:00 +0200

    in

---
+++ Task 19/js/script.js
@@ -0,0 +1,11 @@
+// function rowSumOddNumbers(n) {
+//     return n**3
+// }
+const rowSumOddNumbers = n => n**3
+
+console.log(rowSumOddNumbers(2));
+console.log(rowSumOddNumbers(3));
+console.log(rowSumOddNumbers(4));
+console.log(rowSumOddNumbers(42))
+
+
