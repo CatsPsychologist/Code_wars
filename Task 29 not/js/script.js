ea0797dcaa85e172f56ccb4af6111e751ac5910e
CatsPsychologist <mainofficee@gmail.com>
2022-01-23 11:04:42 +0200

    in

---
+++ Task 29 not/js/script.js
@@ -0,0 +1,9 @@
+function abbrevName(name){
+
+    let first = name[0].splice(0, 1);
+    console.log(first)
+
+}
+
+console.log(abbrevName("Sam Harris"));
+console.log(abbrevName("patrick feenan"));
