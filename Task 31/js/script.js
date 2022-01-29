42a91ad9db39b8eaea5ad14a6e95eb866057df4e
CatsPsychologist <mainofficee@gmail.com>
2022-01-29 16:14:38 +0200

    in

---
+++ Task 31/js/script.js
@@ -0,0 +1,14 @@
+function towerBuilder(nFloors) {
+    let arr = [];
+    
+
+
+    // arr.length === nFloors ? return arr :
+    if (arr.length === nFloors){
+
+        return arr
+    }
+}
+
+console.log(towerBuilder(1));
+console.log(towerBuilder(2));
