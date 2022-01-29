64e000e7e2c34393ae0fc9657ec5e117656d7a0c
CatsPsychologist <mainofficee@gmail.com>
2022-01-29 16:11:23 +0200

    in

--- Task 30 Pangram/js/script.js
+++ Task 30 Pangram/js/script.js
@@ -1,12 +1,44 @@
-function isPangram(str){
-    let alphabet = 'qwertyuiopasdfghjklzxcvbnm';
-    let low = str.toLowerCase();
+// function isPangram(str){
+//     let alphabet = 'qwertyuiopasdfghjklzxcvbnm';
+//     let low = str.toLowerCase();
+//
+//     // str.includes(alphabet) ? true : false;
+//     console.log(str.includes(alphabet))
+//
+//
+// }
+//
+// console.log(isPangram("The quick brown fox jumps over the lazy dog."))
+// console.log(isPangram("This is not a pangram."))
 
-    // str.includes(alphabet) ? true : false;
-    console.log(str.includes(alphabet))
+// let users = [{age:15, name:'A'}, {age: 10, name:'B'}, {age:25, name:'C'}]
+// function find(user){
+//
+//     let updArr = [];
+//
+//     user.map(value => {
+//         if (value.age > 14){
+//             // console.log(value)
+//             updArr.push(value)
+//         }
+//
+//     })
+//
+//     return updArr
+// }
+//
+// console.log(find(users))
+//
+// console.log(0.1 + 0.2)
+
+// function comp(a , b){
+//
+//     return a > b ? b : a ;
+//
+// }
+//
+// console.log(comp(1, 2));
+// console.log(comp(2, 1));
 
 
-}
 
-console.log(isPangram("The quick brown fox jumps over the lazy dog."))
-console.log(isPangram("This is not a pangram."))
