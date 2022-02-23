afc1a327dea97552dc22f19628e00b1df1d09333
CatsPsychologist <mainofficee@gmail.com>
2022-02-23 17:12:10 +0200

    in

--- Task 37/js/script.js
+++ Task 37/js/script.js
@@ -1,6 +1,24 @@
 function century(year) {
-    // Finish this :)
-    return year;
+    let arr = year.toString().split('')
+    // console.log(arr)
+    let firSec = arr[0] + arr[1];
+    let thFor = +arr[2] + +arr[3];
+    // console.log(firSec, thFor)
+
+    if(year.length === 2){
+        return 1
+    }
+
+        if(thFor === 0){
+            return +firSec
+        }else{
+            return +firSec + 1
+        }
+
+    // if(year.length === 3){
+    //     return 2
+    // }
+
 
 }
 
@@ -8,4 +26,7 @@ console.log((century(1705)));
 console.log((century(1900)));
 console.log((century(1601)));
 console.log((century(2000)));
-// console.log((century(89)));
+console.log((century(89)));
+console.log(century(523))
+//
+// console.log(1)
