fc568ed4a146d0da875ca97e661e745768e1a60b
CatsPsychologist <mainofficee@gmail.com>
2022-02-23 17:01:00 +0200

    in

---
+++ Task 37/js/script.js
@@ -0,0 +1,11 @@
+function century(year) {
+    // Finish this :)
+    return year;
+
+}
+
+console.log((century(1705)));
+console.log((century(1900)));
+console.log((century(1601)));
+console.log((century(2000)));
+// console.log((century(89)));
