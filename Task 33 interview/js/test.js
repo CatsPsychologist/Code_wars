8f5b5d00388079753bed759568e3213352634f90
CatsPsychologist <mainofficee@gmail.com>
2022-05-24 22:48:01 +0300

    in

--- Task 33 interview/js/test.js
+++ Task 33 interview/js/test.js
@@ -6,30 +6,44 @@ let arr = [
     {id: 40, parent: 20}
 ]
 
+let arr2 = [
+    {id: 10, parent:[
+            {id: 20, parent: 10},
+            {id: 30, parent: 10}
+        ]
+]
+
 
 function dom(arg){
 
     let result = {};
 
-    // arr.map((value, index) => )
-    return arr.map((value, index) => {
-        console.log(value)
-        if(value.parent === null){
-            let a = value
-            result = a
-            console.log(result)
-        }
+     arr.map((value, index) => {
+        console.log(value.id, value.parent)
 
+
+        if(value.parent === null){
+
+            let a = value;
+            result = a;
+            // console.log(result)
+            // console.log(value.id, value.parent)
+        }
+       if (value.parent === value.id){
+
+            result.value = value
+
+        }
 
 
     })
 
 
-
+    return result
 }
 
 console.log(dom(arr))
-console.log(1)
+console.log(34)
 // let arr2 =
 // {
 //     id: 10, parent: null,
