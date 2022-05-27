30c309d7d5149d1c7cdef30026ce1838a347ae9b
CatsPsychologist <mainofficee@gmail.com>
2022-05-27 22:20:54 +0300

    in

---
+++ Task 42/js/test.js
@@ -0,0 +1,24 @@
+function reverseWords(str) {
+
+    let result = []
+    str.split(' ')
+        .map(value => {
+
+            let word = (value.split('').reverse().join(''))
+            result.push(word)
+        })
+
+
+    return result.join(' ')
+}
+
+
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.'));
+// console.log(reverseWords('apple'));
+// console.log(reverseWords('a b c d'));
+console.log(12)
+
+function petriop = arg =>{
+
+    let resurl = [].strs.ppgjd
+}
